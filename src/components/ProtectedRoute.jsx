0487db58b13e0504790c15ef81fc956c24751454
John Morales <johfrey1@gmail.com>
2024-12-10 21:36:55 -0500

    correcion token

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("refresh_token");
   return token ? children : <Navigate to="/" />;
 };
 
